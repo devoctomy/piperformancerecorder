5177fda213286f08041668cd47604dbf6a2f468b
Nick Pateman <nick@devoctomy.com>
2021-04-12 13:23:04 +0100

    Can now enable / disable video option

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ import time
 import dothat.backlight as backlight
 import dothat.lcd as lcd
 import dothat.touch as nav
-from dot3k.menu import Menu, MenuOption
+from dot3k.menu import Menu, MenuIcon, MenuOption
 
 sys.path.append('.')
 from plugins.clock import Clock
@@ -16,22 +16,76 @@ from plugins.wlan import Wlan
 
 class Video(MenuOption):
     def __init__(self):
+        self.enabled = True
+        self._icons_setup = False
         MenuOption.__init__(self)
-    def redraw(self, menu):
-        menu.write_row(0, 'Video Options')
-        menu.clear_row(1)
-        menu.clear_row(2)
 
-video = Video()
+    def right(self):
+        self.enabled = True
+        self.update_enabled()
+        return True
+
+    def left(self):
+        self.enabled = False
+        self.update_enabled()
+        return True
+
+    def setup_icons(self, menu):
+        menu.lcd.create_char(0, MenuIcon.arrow_left_right)
+        self._icons_setup = True
+
+    def cleanup(self):
+        self._icons_setup = False
+
+    def setup(self, config):
+        self.config = config
+        self.enabled = bool(self.get_option('Video', 'enabled', True))
+
+    def update_enabled(self):
+        self.set_option('Video', 'enabled', str(self.enabled))
+
+    def redraw(self, menu):
+        if not self._icons_setup:
+            self.setup_icons(menu)
+
+        menu.write_row(0, 'Video')
+        menu.write_row(1, chr(0) + 'Enabled: ' + str(self.enabled))
+        menu.clear_row(2)
 
 class Performance(MenuOption):
     def __init__(self):
+        #self._icons_setup = False
         MenuOption.__init__(self)
-    def redraw(self, meunu):
+
+    #def right(self):
+    #    self.update_enabled()
+    #    return True
+
+    #def left(self):
+    #    self.update_enabled()
+    #    return True
+
+    #def setup_icons(self, menu):
+    #    self._icons_setup = True
+
+    #def cleanup(self):
+    #    self._icons_setup = False
+
+    def setup(self, config):
+        self.config = config
+
+    #def update_enabled(self):
+    #    self.set_option('Video', 'enabled', str(self.enabled))
+
+    def redraw(self, menu):
+        #if not self._icons_setup:
+        #    self.setup_icons(menu)
+
         menu.write_row(0, 'Performance')
-        menu.clear_row(1)
+        menu.write_row(1, 'Recording...')
         menu.clear_row(2)
 
+video = Video()
 performance = Performance()
 
 menu = Menu(
